880d3732c05bba76b673c4533f68219231165923
viktor <87v87@mail.ru>
2024-12-18 20:26:04 +0300

    123

--- FitLife/АпппФайл/FitLifeApp.swift
+++ FitLife/АпппФайл/FitLifeApp.swift
@@ -6,19 +6,9 @@
 //
 
 import SwiftUI
-import OpenFoodFactsSDK
 
 @main
 struct FitLifeApp: App {
-    init() {
-        // Конфигурация OpenFoodFactsSDK
-        OFFConfig.shared.apiEnv = .production // Production окружение
-        OFFConfig.shared.productsLanguage = .RUSSIAN // Язык продуктов
-        OFFConfig.shared.userAgent = UserAgent(name: "YourAppName", version: "1.0", system: "iOS", comment: "YourAppComment")
-
-        // Установите глобального пользователя, если необходима авторизация
-  //      OFFConfig.shared.globalUser = User(userId: "yourUsername", password: "yourPassword")
-    }
     var body: some Scene {
         WindowGroup {
             ContentView()
